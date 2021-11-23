474f586d3c241acd0ae2569589727d0d26ad992e
Steve KOUNA <stevekouna@gmail.com>
2021-11-23 15:27:14 +0400

    init

---
+++ src/main/java/com/koona/invoise/product/ProductApplication.java
@@ -0,0 +1,22 @@
+package com.koona.invoise.product;
+
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EntityScan("com.koona.invoise.core.entity.product")
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+	@Bean
+	public Hibernate5Module dataTypeHibernateModule() {
+		return new Hibernate5Module();
+	}
+
+}
